c0bf3fded4ea8533daece25ae2d38376905ccc03
Yura <yura.tolochkevich@gmail.com>
2018-05-17 19:19:42 +0300

    add cluster docker-compose

---
+++ it/src/main/test/org/thingsboard/it/BaseTest.java
@@ -0,0 +1,15 @@
+package org.thingsboard.it;
+
+import org.junit.Test;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
+
+public class BaseTest {
+    @Test
+    public void testFindAssetsByTenantId() {
+        assertEquals(0,0);
+    }
+}
